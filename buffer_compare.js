7a15869ffeed2cc3da2a00cd63f8168553b1f56c
Harshdeep Singh <harshdeeps269@gmail.com>
2021-07-12 17:38:25 +0530

    Add files via upload

---
+++ buffer_compare.js
@@ -0,0 +1,10 @@
+var buffer1 = new Buffer.from('WXYZW');
+var buffer2 = new Buffer.from('WXYZ');
+var result = buffer1.compare(buffer2);
+if(result < 0){
+    console.log(buffer1 + " comes before " + buffer2);
+}else if(result === 0){
+    console.log(buffer1 + " is same as " + buffer2);
+} else {
+    console.log(buffer1 + " comes after " + buffer2);
+}
